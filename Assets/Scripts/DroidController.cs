74cb8a8d4ba1b978a4618731e370e340dd23820e
Luka Signe--Morice <luka@signemorice.com>
2023-11-26 18:47:48 +0100

    Droid moving when pressing button

--- Assets/Scripts/DroidController.cs
+++ Assets/Scripts/DroidController.cs
@@ -7,10 +7,42 @@ public class DroidController : MonoBehaviour
     public AudioSource audioSource;
     private bool isStarted = false;
 
+    public delegate void DroidStart();
+    public event DroidStart OnDroidStart;
+
+    public WaypointMover waypointMover;
+    private Animator animator;
+
+    public delegate void MovementEnded();
+    public event MovementEnded OnMovementEnded;
+
+    [Range(0f, 5f)]
+    public float movementDelay = 1f;
+
+    private void Start()
+    {
+        waypointMover.OnMovementEnded += HandleMovementEnded;
+        animator = GetComponent<Animator>();
+    }
     public void StartDroid()
     {
         if (isStarted) return;
         audioSource.Play();
+        animator.SetTrigger("TrIdle");
         isStarted = true;
+        OnDroidStart?.Invoke();
+        Invoke("startMovement", movementDelay);
+    }
+
+    private void startMovement()
+    {
+        Debug.LogWarning("startmvmt");
+        waypointMover.enabled = true;
+    }
+
+    private void HandleMovementEnded()
+    {
+        animator.SetTrigger("TrOpenArm");
+
     }
 }
--- Assets/Scripts/RoomTwoManager.cs
+++ Assets/Scripts/RoomTwoManager.cs
@@ -6,12 +6,16 @@ public class RoomTwoManager : MonoBehaviour
 {
     public CardReader cardReader;
     public GameObject smallDoor;
+    public DroidController droidController;
+
+    private bool isDroidStarted = false;
 
     private bool isSmallDoorOpened = false;
     // Start is called before the first frame update
     void Start()
     {
         cardReader.OnCardRead += HandleCardRead;
+        droidController.OnDroidStart += HandleDroidStart;
     }
 
     private void HandleCardRead()
@@ -21,6 +25,11 @@ public class RoomTwoManager : MonoBehaviour
         Animator smallDoorAnimation = smallDoor.GetComponent<Animator>();
         smallDoorAnimation.enabled = true;
         smallDoorAnimation.Play("glass_door_open");
+    }
+
+    private void HandleDroidStart()
+    {
+        isDroidStarted = true;
     }
 
     // Update is called once per frame
